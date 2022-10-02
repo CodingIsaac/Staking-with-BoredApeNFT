ed4af72af33b59c5f8237af37a9749275f6b76a4
CodingIsaac <isaacijuo@gmail.com>
2022-10-02 16:36:13 +0000

    Updated Commit

--- contracts/staking.sol
+++ contracts/staking.sol
@@ -16,7 +16,6 @@ contract Staking {
     struct Info {
         uint256 amount;
         uint256 noOfdays;
-        
     }
 
     mapping(address => Info) stakes;
@@ -34,23 +33,37 @@ contract Staking {
     //     _;
     // }
 
-    function stakeTokens( uint _amount) external {
+    function stakeTokens(uint256 _amount) external {
         require(_amount > 0, "Zero Token can't be staked");
-        require(IERC20(kejiToken).balanceOf(msg.sender) >= _amount, "Insufficient Balance");
-        require(IERC721(boredApe).balanceOf(msg.sender) > 0, "You must own a Bored Ape before you can stake");
+        require(
+            IERC20(kejiToken).balanceOf(msg.sender) >= _amount,
+            "Insufficient Balance"
+        );
+        require(
+            IERC721(boredApe).balanceOf(msg.sender) > 0,
+            "You must own a Bored Ape before you can stake"
+        );
 
         IERC20(kejiToken).transferFrom(msg.sender, address(this), _amount);
         Info storage stakingData = stakes[msg.sender];
         stakingData.amount = _amount;
         stakingData.noOfdays = block.timestamp;
-
-
-
-    }
-    function safeWithdrawal() public {}
-
-    function stakingCommission() private view returns (uint256 commission) {
-        commission = (address(this).balance * 100) / 10000;
     }
 
+    function safeWithdrawal() external {
+        
+    }
+
+    function stakingCommission() public  {
+        Info memory stakingData = stakes[msg.sender];
+        uint balance = stakingData.amount;
+        require(balance > 0, "Insufficient Balance");
+        require(IERC20(kejiToken).balanceOf(address(this)) >= balance, "Insufficient Balance");
+        uint lockedTime = block.timestamp - stakingData.noOfdays;
+        uint cumulativeProfit = ((balance / 42000000) * lockedTime);
+        uint withdrawal = cumulativeProfit + balance;
+        stakingData.amount = 0;
+        IERC20(kejiToken).transfer(msg.sender, withdrawal);
+
+    }
 }
